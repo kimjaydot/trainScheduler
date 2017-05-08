5d961f6dbb5d2ae708cdad0ee9ac5a6b254dd75b
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-05-07 21:01:07 -0500

    organized folders and files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
- 
+     
    // Initialize Firebase
   var config = {
     apiKey: "AIzaSyD9stvNwuJylhixbZ6mPDlhz-VSAyjCJ3A",
@@ -18,15 +18,40 @@
     var firstTrainName = "";
     var frequency = "";
 
+    //test Momement Code
+
+    // var tFrequency = 3;
+    // // Time is 3:30 AM
+    // var firstTime = "03:30";
+    // // First Time (pushed back 1 year to make sure it comes before current time)
+    // var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+    // console.log(firstTimeConverted);
+    // // Current Time
+    // var currentTime = moment();
+    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    // // Difference between the times
+    // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    // console.log("DIFFERENCE IN TIME: " + diffTime);
+    // // Time apart (remainder)
+    // var tRemainder = diffTime % tFrequency;
+    // console.log(tRemainder);
+    // // Minute Until Train
+    // var tMinutesTillTrain = tFrequency - tRemainder;
+    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    // // Next Train
+    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
        // On Click Submit Button
-    $("#submitBtn").on("click", function(event) {
+    $("#submit-button").on("click", function(event) {
       event.preventDefault();
       // Grabbed values from text boxes
-      trainName = $("trainName-input").val().trim();
-      destination = $("destination-input").val().trim();
+      trainName = $("#trainName-input").val().trim();
+      destination = $("#destination-input").val().trim();
       firstTrainName = $("#firstTrain-input").val().trim();
       frequency = $("#frequency-input").val().trim();
-      // Code for handling the push
+      // Push information
       
       database.ref().push({
         trainName: trainName,
@@ -38,31 +63,16 @@
       });
     });
 
-          // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+      // Firebase watcher + initial loader 
     database.ref().on("child_added", function(childSnapshot) {
-      // Log everything that's coming out of snapshot
+      // Logs snapshot
       console.log(childSnapshot.val().trainName);
       console.log(childSnapshot.val().destination);
       console.log(childSnapshot.val().firstTrainName);
       console.log(childSnapshot.val().frequency);
       console.log(childSnapshot.val().dateAdded);
     
-      // 
+      // Making Changes to Table
       $(".trainSchedule").append("<tr><td>" + childSnapshot.val().trainName + "</td><td>" + childSnapshot.val().destination  + "</td><td>" + childSnapshot.val().firstTrainName + "</td><td>" + childSnapshot.val().frequency  + "</td><td>" + childSnapshot.val().dateAdded  + "</td></tr>"
        );
-  }, function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
-    });
-    dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-      // Change the HTML to reflect
-      $("#name-display").html(snapshot.val().name);
-      $("#email-display").html(snapshot.val().email);
-      $("#age-display").html(snapshot.val().age);
-      $("#comment-display").html(snapshot.val().comment);
-    });
-
-
-
-
-
-
+  }); 
